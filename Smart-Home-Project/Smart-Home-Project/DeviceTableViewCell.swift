a9dfc3a0326e44f814a902946636b33fd8b6a2ff
sguiscardo <sdguiscardo@gmail.com>
2023-02-15 17:22:33 -0600

    Created aler pop up

---
+++ Smart-Home-Project/Smart-Home-Project/DeviceTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  DeviceTableViewCell.swift
+//  Smart-Home-Project
+//
+//  Created by Sebastian Guiscardo on 2/15/23.
+//
+
+import UIKit
+
+class DeviceTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
